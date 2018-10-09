b4299a801eefbfc6b431b2efff557d991ab4cee4
DeadRed6 <21145991+DeadRed6@users.noreply.github.com>
2018-10-09 20:00:01 +0100

    Complete I3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
 	root to: 'articles#index'
 	resources :articles do #expect requests for this resource to be RESTful
 		resources :comments #indicate one to many relation
 	end
+	resources :tags
 end
