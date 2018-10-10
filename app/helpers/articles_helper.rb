b76c023810427c357f108e567c02411328da0340
DeadRed6 <21145991+DeadRed6@users.noreply.github.com>
2018-10-10 20:18:16 +0100

    Complete I4

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,6 @@
 module ArticlesHelper
 	def article_params
-		params.require(:article).permit(:title, :body, :tag_list) #accept only certain attributes
+		params.require(:article).permit(:title, :body, :tag_list, :image) #accept only certain attributes
 	end
 
 end
